71cff9eca872540638be81d6acfa0e422cbb2353
Verlebie <32109081+jiroyu@users.noreply.github.com>
2022-06-24 16:46:28 +0800

    add more chance to throw

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['F', 'T', 'L', 'R', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T']
 
 @app.route("/", methods=['GET'])
 def index():
